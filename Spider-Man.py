44a703581f88f9181bebf6bddd738190a3df72b7
norman-brooks <nbprogramming@outlook.com>
2025-03-17 10:07:39 -0700

    commit 3/17

---
+++ Spider-Man.py
@@ -0,0 +1,8 @@
+answer = "Electro"
+if answer == "Spider-Man":
+    print("You chose Peter Parker")
+elif answer == "Green Goblin":
+    print("You chose Norman Osbourne")
+else:
+    print("Please choose Spider-Man or Green Goblin as your answer")
+    
---
+++ Tuple.py
@@ -0,0 +1,7 @@
+tuple = ("Batman", "Spider-Man", "Wolverine", "Superman")
+a, b, c, d = tuple
+print(a)
+print(b)
+print(c)
+print(d)
+
